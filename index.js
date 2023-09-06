c1374c5c9d37b6da0483398346f47195dcb92fce
Sarah-2002 <sarah17222002@gmail.com>
2023-09-06 15:17:52 +0300

    added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex") ({
+    client: "pg",
+    connection:
+      "postgres://postgres:2212002123@test-database.cfuddmowxczu.us-east-1.rds.amazonaws.com/postgres?ssl=true",
+  });
+
+
+  const app = express ();
+
+  const port = 3000 ;
+
+  // middleware to use cors paackage where origin is any route theird part etc.. //
+
+  app.use(cors({ origin: "*" })); 
+
+
+  // middleware for deseralizing //
+  app.use(express.json());
+  
+
+  // middleware for authetnication //
+
+  // step 1 : install jsonwebtoken and bcrypt : they hash passwords //
+
+  //step 2 : 
